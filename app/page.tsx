1e57ee5a4cdb69003c853cc37960ad5fdf414c83
kentaro23 <kentaro23@users.noreply.github.com>
2026-02-27 14:15:06 +0900

    Make root route always redirect to login to avoid runtime crash on /

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import { redirect } from "next/navigation";
-import { requireUser, getAccessibleSocieties } from "@/lib/session";
 
-export default async function HomePage() {
-  const user = await requireUser();
-  const memberships = await getAccessibleSocieties(user.id);
-  if (memberships[0]) {
-    redirect(`/t/${memberships[0].societyId}`);
-  }
-  redirect("/admin/societies");
+export default function HomePage() {
+  redirect("/login");
 }
